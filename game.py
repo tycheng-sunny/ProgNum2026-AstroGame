495c3769ee8e4f84331ec6341172928185a7cbe2
Adrià <taiga@Ades-MacBook-Air.local>
2026-04-02 00:23:01 +0200

    beam return added

--- game.py
+++ game.py
@@ -7,6 +7,10 @@ import turtle
 import math
 import random
 
+galaxy_catalogue = {
+
+}
+
 # Screen Set-up
 screen = turtle.Screen()
 screen.bgcolor('black')
@@ -66,39 +70,42 @@ beam.speed(0)
 beam.shapesize(.3,.3)
 beam.hideturtle()
 
-beamspeed = 20
+beamspeed = 30
 
 #Beam state
 #ready - ready to shoot
 #shoot - beam is being shot
 beamstate = 'ready'
 
+class Player:
+    def __init__(self):
+        ...
 
-#Player actions
-def move_left():
-    x = player.xcor()
-    x -= playerspeed
-    if x < -280:   #Stops player from going off-screen
-        x = -280
-    player.setx(x)
-
-def move_right():
-    x = player.xcor()
-    x += playerspeed
-    if x > 280:   #Same as before
-        x = 280
-    player.setx(x)
-
-def fire_beam():
-    #Make beamstate a global if it needs changes
-    global beamstate
-    if beamstate == 'ready':
-        beamstate = 'fire'
-        #Move beam to just above the player
+    #Player actions
+    def move_left(self):
         x = player.xcor()
-        y = player.ycor() + 10
-        beam.setposition(x,y)
-        beam.showturtle()
+        x -= playerspeed
+        if x < -280:   #Stops player from going off-screen
+            x = -280
+        player.setx(x)
+
+    def move_right(self):
+        x = player.xcor()
+        x += playerspeed
+        if x > 280:   #Same as before
+            x = 280
+        player.setx(x)
+
+    def fire_beam(self):
+        #Make beamstate a global if it needs changes
+        global beamstate
+        if beamstate == 'ready':
+            beamstate = 'fire'
+            #Move beam to just above the player
+            x = player.xcor()
+            y = player.ycor() + 10
+            beam.setposition(x,y)
+            beam.showturtle()
 
 def isCollision(t1, t2):
     distance = math.sqrt(math.pow(t1.xcor() - t2.xcor(), 2) + math.pow(t1.ycor() - t2.ycor(), 2))
@@ -108,10 +115,11 @@ def isCollision(t1, t2):
         return False
 
 #Keyboard bindings
+P = Player()
 turtle.listen()
-turtle.onkey(move_left, 'Left')
-turtle.onkey(move_right, 'Right')
-turtle.onkey(fire_beam, 'space')
+turtle.onkey(P.move_left, 'Left')
+turtle.onkey(P.move_right, 'Right')
+turtle.onkey(P.fire_beam, 'space')
 
 #Main game loop
 while True:
@@ -137,11 +145,9 @@ while True:
 
         # Check for collision
         if isCollision(beam, galaxy):
-            # Reset beam
-            beam.hideturtle()
-            beamstate = 'ready'
-            beam.setposition(0, -400)
-            # Catalogue galaxy
+            #Make beam come back
+            beamspeed *= -1
+            #Replace galaxy
             x = random.randint(-200,200)
             y = random.randint(-200,-100)
             galaxy.setposition(x, y)
@@ -156,10 +162,14 @@ while True:
         y += beamspeed
         beam.sety(y)
 
+    #Reset beam and catalogue galaxy
+    if beam.ycor() < -280:
+        beam.hideturtle()
+        beamstate = 'ready'
+        beam.setposition(0, -270)
+        beamspeed *= -1
+
     #Check whether beam is in-bounds
     if beam.ycor() > 270:
         beam.hideturtle()
         beamstate = 'ready'
-
-input('Press Enter to continue...')
-screen.exitonclick()
