ee2d5509691da6570f2f8d0389d6b2ae57f2869f
tycheng <52981857+tycheng-sunny@users.noreply.github.com>
2026-05-08 11:12:04 +0100

    Update two new games and corresponding documents

--- glossary.py
+++ glossary.py
@@ -7,11 +7,12 @@ GAMES = {
     "Asteroid Game": "game-DAC/",
     "Black Hole Game": "game-SL/",
     "Colour Matcher": "game-MG/",
+    "Constellations Quiz": "game-VCT/",
     "Medicine across the Universe": "game-LS/",
     "Memorising Galaxies": "game-LK/",
     "Save the Ship": "game-MB/",
     "Space Rescue Game": "game-ND/",
-    "test4": "test",
+    "Sweet Saviour": "game-AV/",
     "test5": "test",
     "test6": "test",
     "test7": "test",
@@ -29,18 +30,32 @@ AUTHOR_NAMES = {
     "Asteroid Game": "D. Alvarez Corres",
     "Black Hole Game": "S. Leszek",
     "Colour Matcher": "M. Gerritsen",
+    "Constellations Quiz": "V. Calvo Tenza",
     "Medicine across the Universe": "L. Suberbere",
     "Memorising Galaxies": "L. Klingens",
     "Space Rescue Game": "N. Demmenie", 
-    "Save the Ship": "M. de Boer",
+    "Save the Ship": "M. Boer",
+    "Sweet Saviour": "A. Vecht",
 }
 
 GAME_INFO = {
     "Asteroid Game": "Collect belts while avoiding asteroids that reduce your health. The game ends when your health reaches 0.",
     "Black Hole Game": "Game inspired by pacman - player uses arrow keys to steer their black hole and eat stars, gathering their mass. User can eat other black holes as long as their black hole has bigger mass. Other black holes are like ghosts in pacman - they can eat you! Win when user absorbs all black holes or stars, or has the most mass of all objects.",
     "Colour Matcher": "In this game, you need to get the same color values as given to you, at the upmost left corner there is a hue that you need to match. (here you can also see the percentage of pixels that are that color). Just move your mouse over the image to get values in the graph and get them inside the range. You can get a new color if this one is too hard or you cannot find it.",
-    "Medicine across the Universe": "You are on Pluto and need to go to Mercury. Take your ship and you have to make decisions on what do to. Moving to another planet will take you electricity. Everyday you need to eat. You can produce food but it needs electricity. Be careful, if you run out of electricity, you cannot move anymore and are stuck in space forever but if you run out of food, you will die. And the clock is ticking, so don't take too much your time. \n\n How to play: it is very intuitive : just answer the questions and make decision based on what is happening. IMPORTANT: for the game to be running please input the available options only, otherwise you will just skip to the next day.",
+    "Constellations Quiz": "Before playing this little quiz, make sure you are an astronomy lover, above all a constellations freak. Once you satisfy the most important condition, you can start playing it to challenge yourself on your stars knowledge. \n\n"
+        "Instructions: \n "
+        "1. You will be given a random main star of a constellation \n "
+        "2. Try to guess the constellation to win points \n "
+        "3. Once you guessed t correctly, you can see the constellation \n "
+        "4. Then, repeat until you have learnt all of them \n\n"
+        "Good luck guessing and have fun!",
+    "Medicine across the Universe": "You are on Pluto and need to go to Mercury. Take your ship and you have to make decisions on what do to. Moving to another planet will take you electricity. Everyday you need to eat. You can produce food but it needs electricity. Be careful, if you run out of electricity, you cannot move anymore and are stuck in space forever but if you run out of food, you will die. And the clock is ticking, so don't take too much your time. \n\n"
+        "HOW TO PLAY: \n    ""It is very intuitive : just answer the questions and make decision based on what is happening. \n\n"
+        "IMPORTANT: \n    ""For the game to be running please input the available options only, otherwise you will just skip to the next day.",
     "Memorising Galaxies": "You have departed Earth and are in a space ship (your call name is Anaconda 7) on a journey. You receive a mission from Earth to find the coordinates of a galaxy, so you can travel to it. By matching two galaxies together, you can obtain the correct coordinates. At the end, you have to enter the coordinates you found. If the coordinates are correct, you can continue your space life as normal, waiting on the next mission. If the coordinates are wrong, you have to restart the game! So be very careful when typing and pay close attention to the mission description!",
     "Space Rescue Game": "Space Rescue is a text-based game where you travel between planets to rescue stranded astronauts. Each trip costs fuel based on distance, and planetary conditions can damage your ship. You collect points and may encounter random events that help or harm you. The goal is to rescue enough astronauts within a limited number of turns while managing your fuel and health.", 
     "Save the Ship": "In this text-based adventure game, you are in your spacecraft, when the alarms suddenly go off. When you wake up you find out that the ship's systems are failing causing it to become destabilized. The only way you can survive is by restoring the spacecraft yourself. Navigate your way through the ship and solve puzzles to ensure it doesn't go down!",
+    "Sweet Saviour": "Welcome to SWEET SAVIOUR! \n\n " 
+        "A story based astronomy game in which you are tasked to save the world. \n\n"
+        "INSTRUCTIONS: During the story's progression you will be presented with questions and dilemma's. Answer correctly or there might be consequences. In total there are 3 differend endings, try to get them all!",
 }
